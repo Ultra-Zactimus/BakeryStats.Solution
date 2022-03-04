f2a34052c92e08013fc15dc6f635230c66958675
Ultra Zacky <ultrazacky@gmail.com>
2022-03-04 09:47:55 -0800

    add husk of test for Vendor class

---
+++ BakeryStats.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using BakeryStatistics.Models;
+
+namespace BakeryStatistics.Tests
+{
+  [TestClass]
+  public class VendorTests
+  {
+    [TestMethod]
+    public void Vendor_ShouldCreateInstanceOfObject_NewVendor()
+    {
+      
+    }
+  }
+}
