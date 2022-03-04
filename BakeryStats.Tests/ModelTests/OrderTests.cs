77529372f8939575ca79dbebc5cbab953dff03b8
Ultra Zacky <ultrazacky@gmail.com>
2022-03-04 08:21:11 -0800

    test Order object to make sure it gets a good fail

---
+++ BakeryStats.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using BakeryStatistics.Models;
+
+namespace BakeryStatistics.Tests
+{
+  [TestClass]
+  public class OrderTests
+  {
+    [TestMethod]
+    public void Order_ItShouldCreateInstanceOfObject_NewOrder()
+    {
+      Order order = new Order("blah", "blahblah", 0, 0);
+      Assert.AreEqual(typeof(string), order.GetType());
+    }
+  }
+}
