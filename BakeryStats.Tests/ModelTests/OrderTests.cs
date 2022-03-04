78e94fa58688cdb77c739fc8761ef83d3ac28788
Ultra Zacky <ultrazacky@gmail.com>
2022-03-04 09:06:43 -0800

    add test for testing strings in object, good fail

--- BakeryStats.Tests/ModelTests/OrderTests.cs
+++ BakeryStats.Tests/ModelTests/OrderTests.cs
@@ -11,8 +11,17 @@ namespace BakeryStatistics.Tests
     [TestMethod]
     public void Order_ItShouldCreateInstanceOfObject_NewOrder()
     {
-      Order order = new Order("blah", "blahblah", 0, 0);
+      Order order = new Order("blah", "blahblah", 0, 2022);
       Assert.AreEqual(typeof(Order), order.GetType());
+    }
+
+    [TestMethod]
+    public void Order_NewObjectShouldContainItsValues_String()
+    {
+      string name = "John";
+      Order order = new Order(name, "string", 0, 0);
+      string newOrder = order.Customer;
+      Assert.AreEqual("jimbob", newOrder);
     }
   }
 }
